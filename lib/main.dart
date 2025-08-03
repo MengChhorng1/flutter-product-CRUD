912b513d300d9b6711cebd822f9a1b6534243b47
MengChhorng1 <chhorng349@gmail.com>
2025-08-03 14:08:01 +0700

    My product CRUD Project

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+
+import 'providers/product_provider.dart';
+import 'screens/product_list_screen.dart';
+import 'services/api_service.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => http.Client()),
+        ChangeNotifierProvider(
+          create: (context) => ProductProvider(
+            apiService: ApiService(client: context.read<http.Client>()),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Product CRUD',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            centerTitle: true,
+          ),
+        ),
+        home: const ProductListScreen(),
+      ),
+    );
+  }
+}
